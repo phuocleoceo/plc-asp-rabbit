77400edb9f0b6a4ec36bafab0f7142d8ef06d9bc
phuocleoceo <ht10082001@gmail.com>
2024-03-24 14:59:54 +0700

    Producer

--- PlcRabbitLibrary/RegisterServiceExtensions.cs
+++ PlcRabbitLibrary/RegisterServiceExtensions.cs
@@ -1,3 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
+using PlcRabbitLibrary.Configuration;
+using PlcRabbitLibrary.Producer;
+
 namespace PlcRabbitLibrary;
 
-public static class RegisterServiceExtensions { }
+public static class RegisterServiceExtensions
+{
+    public static IServiceCollection AddKafkaProducer<T>(
+        this IServiceCollection services,
+        Action<RabbitProducerConfig> configAction
+    )
+    {
+        services.Configure(configAction);
+        services.AddSingleton(typeof(IRabbitProducer<>), typeof(RabbitProducer<>));
+        return services;
+    }
+}
